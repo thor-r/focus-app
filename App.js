fd43ca7f557fdbf088a877b078a75d123d6dc48f
thor-r <twilliamrefoy@gmail.com>
2022-07-03 17:36:51 +0100

    first push

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, Platform, SafeAreaView, StatusBar } from 'react-native'
+import Constants from 'expo-constants'
+import { colors } from './src/utils/colors'
+import { Focus } from './src/features/Focus'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    <SafeAreaView style={styles.container}>
+      <Focus />
+
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    backgroundColor: colors.darkBlue,
   },
-});
+})
---
+++ src/utils/colors.js
@@ -0,0 +1,6 @@
+export const colors = {
+  white: '#fff',
+  darkBlue: '#2E4A62',
+
+
+}
